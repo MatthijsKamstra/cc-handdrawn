02eb12e2332b8908a380856794e5521e05ae0afb
Matthijs Kamstra <matthijs@fonk.amsterdam>
2020-02-13 21:42:39 +0100

    added drips

---
+++ src/art/CCDrips.hx
@@ -0,0 +1,74 @@
+package art;
+
+import sketcher.draw.AST.LineCap;
+
+class CCDrips extends SketcherBase {
+	var grid:GridUtil;
+	var circleRadius = 50;
+
+	public function new() {
+		// use debug?
+		this.isDebug = true;
+
+		super();
+	}
+
+	function drawShape() {
+		for (k in 0...grid.array.length) {
+			// center point
+			var cp:Point = grid.array[k];
+			sketch.makeX(Math.round(cp.x), Math.round(cp.y));
+
+			// random biggest spatter
+			var randomCircleRadius = random(circleRadius / 2, circleRadius);
+			// for-ground
+			var circle = sketch.makeCircle(Math.round(cp.x), Math.round(cp.y), Math.round(randomCircleRadius));
+			circle.fill = ' black ';
+			circle.noStroke();
+			// border spatter
+			for (i in 0...10) {
+				var rp:Point = {
+					x: cp.x + random(-randomCircleRadius, randomCircleRadius),
+					y: cp.y + random(-randomCircleRadius, randomCircleRadius)
+				};
+				var spatter = sketch.makeCircle(Math.round(rp.x), Math.round(rp.y), Math.round(random(randomCircleRadius / 2)));
+				spatter.fill = rgb(0); // ' black ';
+				spatter.noStroke();
+			}
+			// drip
+			for (j in 0...3) {
+				var dripWeight = randomInt(10, Math.round(randomCircleRadius / 2));
+				var rp:Point = {
+					x: cp.x + random(-randomCircleRadius + dripWeight, randomCircleRadius - dripWeight),
+					y: cp.y + random(-randomCircleRadius + dripWeight, randomCircleRadius - dripWeight)
+				};
+				// drip line, straight down
+				var line = sketch.makeLine(Math.round(rp.x), Math.round(rp.y), Math.round(rp.x),
+					Math.round(rp.y + random(randomCircleRadius, randomCircleRadius + 100)));
+				line.lineCap = LineCap.Round;
+				line.stroke = rgb(0);
+				line.lineWeight = dripWeight;
+			}
+		}
+
+		// draw
+		sketch.update();
+	}
+
+	// ____________________________________ override ____________________________________
+
+	override function setup() {
+		trace('SETUP :: ${toString()}');
+
+		// to grid
+		grid = new GridUtil(w, h);
+		grid.setIsCenterPoint(true);
+		grid.setNumbered(3, 3);
+	}
+
+	override function draw() {
+		// trace('DRAW :: ${toString()}');
+		drawShape();
+		stop();
+	}
+}
